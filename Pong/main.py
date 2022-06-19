160c7dfcc5b9759eff2bb72cd6d1f4041cd808be
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-06-19 11:17:44 -0500

    Add files via upload

--- Pong/main.py
+++ Pong/main.py
@@ -1,8 +1,15 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.bgcolor("black")
+
+p1_paddle = Paddle()
+
+screen.listen()
+screen.onkey(fun=p1_paddle.up, key="Up")
+screen.onkey(fun=p1_paddle.down, key="Down")
 
 screen.exitonclick()
--- Pong/paddle.py
+++ Pong/paddle.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
 
 class Paddle(Turtle):
     def __init__(self):
@@ -10,23 +10,10 @@ class Paddle(Turtle):
         self.turtlesize(stretch_wid=5, stretch_len=1)
         self.goto(x=350, y=0)
 
-    # def up(self):
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(x=self.xcor(), y=new_y)
 
-
-    # def down(self):
-
-
-    # def left(self):
-
-    
-    # def right(self):
-
-
-screen = Screen()
-screen.setup(width=800, height=600)
-screen.title("Pong")
-screen.bgcolor("black")
-paddle = Paddle()
-
-screen.exitonclick()
-
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(x=self.xcor(), y=new_y)
